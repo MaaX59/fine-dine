e94576decc1522b1ac903385f6f0133b4a8cf9fd
MaaX59 <maax.strandberg@gmail.com>
2023-09-18 19:33:32 +0200

    menu content finished

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import {images, data} from '../../constants';
-import { SubHeading, MenuItem } from '../../components';
+import React from "react";
+import { images, data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 
-
-import './SpecialMenu.css';
+import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div className='app__specialMenu flex__center section__padding' id='menu'>
-  <div className='app__specialMenu-title'>
-  <SubHeading />
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu That Fits Your Palatte" />
+      <h1 className="headtext__cormorant">Today´s Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+        {data.wines.map((wine,index) => (
+          <p>{wine.title}</p>
+        ))}
+          </div>
+      </div>
 
-  </div>
-    
+      <div className="app__specialMenu_menu_img">
+        <img src={images.menu} alt="menu-img"/>
+      </div>
+
+      
+      <div className="app__specialMenu_cocktails flex__center">
+        
+        <div className="app__specialMenu_menu_items">
+        {data.cocktails.map((cocktail,index) => (
+          <p>{cocktail.title}</p>
+        ))}
+          </div>
+          </div>
+
+      
+
+      
+    </div>
+    <div style={{marginTop:'15px'}}>
+      <button type="button" className="custom__button" >View More</button>
+    </div>
   </div>
 );
 
