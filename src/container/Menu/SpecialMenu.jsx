a76252e44c04ce35dcdf837e0a62c5d5b2570ab3
MaaX59 <maax.strandberg@gmail.com>
2023-09-19 18:31:14 +0200

    menu styling

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -15,7 +15,8 @@ const SpecialMenu = () => (
         <p className="app__specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
         {data.wines.map((wine,index) => (
-          <p>{wine.title}</p>
+          <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+          
         ))}
           </div>
       </div>
@@ -26,10 +27,10 @@ const SpecialMenu = () => (
 
       
       <div className="app__specialMenu_cocktails flex__center">
-        
+      <p className="app__specialMenu_menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
         {data.cocktails.map((cocktail,index) => (
-          <p>{cocktail.title}</p>
+          <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
         ))}
           </div>
           </div>
