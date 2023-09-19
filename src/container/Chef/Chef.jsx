bffcdc978a8d675d1fd5fc1ae9e522558d0626d5
MaaX59 <maax.strandberg@gmail.com>
2023-09-19 19:13:30 +0200

    chef content done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import {images} from '../../constants'
-import  {SubHeading} from '../../components'
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
-import './Chef.css';
+import "./Chef.css";
 
 const Chef = () => (
-  <div className='app__bg app__wrapper section__padding'>
-    <div className='app__wrapper_img app_wrapper_img_reverse'>
-<img src={images.chef} alt="chef"/>
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app_wrapper_img_reverse">
+      <img src={images.chef} alt="chef" />
     </div>
-    <div className='app__wrapper_info'>
-    <SubHeading title="chef´s Word" />
-    <h1 className='headtext__cormorant'> What We Belive In</h1>
-
+    <div className="app__wrapper_info">
+      <SubHeading title="chef´s Word" />
+      <h1 className="headtext__cormorant"> What We Belive In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <p className="p__opensans"></p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans"> Chef & Founder</p>
+        <img src={images.sign} alt="chefSign" />
+      </div>
     </div>
   </div>
 );
