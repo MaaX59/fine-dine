4484c02435a9d8dd09da4660280c93fb015ac648
MaaX59 <maax.strandberg@gmail.com>
2023-09-19 19:04:26 +0200

    chef section started

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {images} from '../../constants'
+import  {SubHeading} from '../../components'
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app_wrapper_img_reverse'>
+<img src={images.chef} alt="chef"/>
+    </div>
+    <div className='app__wrapper_info'>
+    <SubHeading title="chef´s Word" />
+    <h1 className='headtext__cormorant'> What We Belive In</h1>
+
+    </div>
   </div>
 );
 
