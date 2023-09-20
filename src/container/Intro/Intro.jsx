1f6423c6dd0404bacf2a6c36e359d410f483064a
MaaX59 <maax.strandberg@gmail.com>
2023-09-20 18:58:24 +0200

    intro styles

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,30 +1,45 @@
-import {React, useRef, useState} from 'react';
-import {BsFillPlayFill, BsPauseFill} from 'react-icons';
-import {meal} from '../../constants';
-import './Intro.css';
+import { React, useRef, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
 
 const Intro = () => {
-const [playVideo, setPlayVideo] = useState(false);
+  const [playVideo, setPlayVideo] = useState(false);
 
-const videoRef = useRef();
-
+  const videoRef = useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  };
 
   return (
-  <div className='app__video'>
-  <video 
-    src={meal}
-    type="video/mp4"
-    loop
-    muted
-    controls={false}
-    ref={videoRef}
-  />
-  <div className='app__video-overlay flex__center'>
-    <div className='app__video-overlay_circle flex__center' > </div>
-  </div>
-    
-  </div>
-  )
-  }
+    <div className="app__video">
+      <video
+        src={meal}
+        type="video/mp4"
+        loop
+        muted
+        controls={false}
+        ref={videoRef}
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
