0487be4cb2e767b07994d7cce86671c2ecd30110
MaaX59 <maax.strandberg@gmail.com>
2023-09-20 19:07:06 +0200

    awards start

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
-import './Laurels.css';
+import React from "react";
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Laurels.css";
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & Recognitions" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
