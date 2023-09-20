619a9f0a2a50abe7fc954d3eb1d54d82f690ddac
MaaX59 <maax.strandberg@gmail.com>
2023-09-20 18:44:05 +0200

    video progress

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import {React, useRef, useState} from 'react';
 import {BsFillPlayFill, BsPauseFill} from 'react-icons';
 import {meal} from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
+const Intro = () => {
+const [playVideo, setPlayVideo] = useState(false);
+
+const videoRef = useRef();
+
+
+  return (
+  <div className='app__video'>
+  <video 
+    src={meal}
+    type="video/mp4"
+    loop
+    muted
+    controls={false}
+    ref={videoRef}
+  />
+  <div className='app__video-overlay flex__center'>
+    <div className='app__video-overlay_circle flex__center' > </div>
+  </div>
     
   </div>
-);
+  )
+  }
 
 export default Intro;
