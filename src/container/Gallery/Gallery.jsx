d3cb8ceac34f026aa26dfeab7da7a1b8cc88ce97
MaaX59 <maax.strandberg@gmail.com>
2023-09-21 19:40:20 +0200

    gallery styles almost done

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -43,6 +43,7 @@ const Gallery = () => {
         <div className="app__gallery-images_container" ref={scrollRef}>
           {galleryImages.map((image, index)=> (
             <div className="app__gallery-images_card flex__center" key={`gallery_image- ${index + 1}`} > 
+            <BsInstagram className="gallery__image-icon" />
             <img src={image} alt="gallery" />
             </div>
           ))}
