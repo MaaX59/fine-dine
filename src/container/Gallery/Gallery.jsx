2ca45ba929c1fdbf8991af6a428ff9e958e85b03
MaaX59 <maax.strandberg@gmail.com>
2023-09-21 19:14:22 +0200

    gallery half done

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
-import { BsInstagram, BsArrowLeftShort, BsArrowRightShort   } from 'react-icons/bs';
-import {images} from '../../components';
-import {SubHeading} from '../../constants';
+import React from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
-import './Gallery.css';
+import "./Gallery.css";
 
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
   
-  
-  return(
-  <div className='app__gallery flex__center'>
-  <div className='app__gallery-content'>
-  <SubHeading  title="Instagram"/>
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+    if (direction === "left") {
+      current.scrollRef -= 300;
+    } else {
+      current.scroll += 300;
+    }
+  };
 
-  </div>
-    
-  </div>
-  )
-}
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+          Le Lorem Ipsum est simplement du faux texte employé dans la
+          composition et la mise en page avant impression. Le Lorem Ipsum est le
+          faux texte standard de l'imprimerie depuis les années 1500, quand un
+          imprimeur anonyme assembla ensemble des morceaux de texte pour
+          réaliser un livre spécimen de polices de texte
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}></div>
+        <div className="app__gallery-images_arrow">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
