7339acb391dc8c87f35280992fb94a2f790e9b43
MaaX59 <maax.strandberg@gmail.com>
2023-09-22 19:36:58 +0200

    footer styles

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FooterOverlay, Newsletter } from "../../components";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
-import {images} from '../../assets';
+import {images} from "../../constants";
 
 import "./Footer.css";
 
@@ -18,8 +18,21 @@ const Footer = () => (
       </div>
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer-logo" />
+        <p className="p__opensans">"The best way to find yourself is to lose yourself in the service of others."</p>
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiInstagram />
+          <FiTwitter />
+        </div>
+        <img src={images.spoon} alt="spoon" className="spoon__img" style={{marginTop:15}} />
       </div>
-      <div className="app__footer-links_work"></div>
+      <div className="app__footer-links_work">
+      <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday - Friday:</p>
+        <p className="p__opensans">10:00 - 21:00</p>
+        <p className="p__opensans">Saturday - Sunday:</p>
+        <p className="p__opensans">10:00 - 01:00</p>
+      </div>
     </div>
     <div className="footer__copyright">
       <p className="p__opensans">2023 Gerícht. All Rights Reserved.</p>
