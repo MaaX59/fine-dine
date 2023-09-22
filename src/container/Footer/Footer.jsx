f5c21b9d3f8506a3a86d99ffc328b25ba1a1911e
MaaX59 <maax.strandberg@gmail.com>
2023-09-22 19:19:09 +0200

    footer more stuff

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { FooterOverlay, Newsletter } from '../../components';
-import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import {images} from '../../assets';
 
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => (
-  <div className='app__footer section__padding' id='login'>
-  <FooterOverlay  />
-  <Newsletter  />
-    <div className='app__footer-links'>
-      <div className='app__footer-links_contact'>
-
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">10 Boulevard de la Chapelle, 75010, Paris</p>
+        <p className="p__opensans">+1 234-567-890</p>
+        <p className="p__opensans">+0 987-654-321</p>
       </div>
-      <div className='app__footer-links_logo'>
-
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer-logo" />
       </div>
-      <div className='app__footer-links_work'>
-
-      </div>
+      <div className="app__footer-links_work"></div>
     </div>
-    <div className='footer__copyright'>
-      <p className='p__opensans'>2023 Gerícht. All Rights Reserved.</p>
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 Gerícht. All Rights Reserved.</p>
     </div>
   </div>
 );
